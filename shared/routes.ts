34c1921683c384ca017a359b4e9f02c9dc899adf
ipkmarketingmty-afk <ipkmarketingmty@gmail.com>
2026-03-07 23:11:45 -0600

    Update routes.ts

--- shared/routes.ts
+++ shared/routes.ts
@@ -7,6 +7,20 @@ export const errorSchemas = {
   internal: z.object({ message: z.string() }),
 };
 
+
+
+delete: {
+  method: 'DELETE' as const,
+  path: '/api/machines/:id' as const,
+  responses: {
+    200: z.custom<typeof machines.$inferSelect>(),
+    404: errorSchemas.notFound,
+  }
+},
+
+
+
+
 export const api = {
   dashboard: {
     stats: {
